0da8e9891cf6cb99bd9b8284e9c17fcaaa625b34
lchipeachy <bvlucia34@gmail.com>
2025-03-28 12:47:50 -0600

    integracion de EmailJs para el envio de correos electronicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input'; 
 import { MatButtonModule } from '@angular/material/button'; 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import emailjs from '@emailjs/browser'; // Importar emailjs
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +10,39 @@ import { Router } from '@angular/router';
 export class ContactComponent {
   constructor(private router: Router) {}
 
+  enviarFormulario(form: any) {
+    // Define los datos del formulario
+    const templateParams = {
+      nombres: form.value.nombres,
+      telefono: form.value.telefono,
+      email: form.value.email,
+      direccion: form.value.direccion,
+      mensaje: form.value.mensaje,
+    };
+
+    // Enviar correo con EmailJS
+    emailjs
+  .send(
+    'service_s8fcqjj', // Reemplaza con tu Service ID
+    'template_tmyulpr', // Reemplaza con tu Template ID
+    templateParams,
+    'CMWTH4Y0D5vdGksWw' // Reemplaza con tu Public Key
+  )
+  .then(
+    (response) => {
+      console.log('Correo enviado con éxito:', response.status, response.text);
+      alert('Correo enviado correctamente.');
+      this.onSubmit(); // Redirige a Home después de enviar el correo
+    },
+    (error) => {
+      console.error('Error al enviar el correo:', error);
+      alert('Hubo un error al enviar el correo. Inténtalo nuevamente.');
+    }
+  );
+
+  }
+
+  // Método para redirigir a la página de inicio
   onSubmit() {
     this.router.navigate(['/home']);  // Redirige a Home
   }
