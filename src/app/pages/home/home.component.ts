11b8ebbc054fae53047003a515f490938ea24fc6
lchipeachy <bvlucia34@gmail.com>
2025-03-28 13:13:55 -0600

    agregando las validaciones de formulario y Emailjs en el formulario de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import emailjs from '@emailjs/browser'; // Importar emailjs
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+  enviarFormulario(form: any) {
+    // Validar que todos los campos requeridos tengan valores
+ if (
+   !form.value.nombres?.trim() || 
+   !form.value.telefono?.trim() || 
+   !form.value.email?.trim() || 
+   !form.value.direccion?.trim() || 
+   !form.value.mensaje?.trim()
+ ) {
+   alert('Por favor, completa todos los campos requeridos antes de enviar el correo.');
+   return; // Detener el envío si hay campos vacíos
+ }
 
+   // Define los datos del formulario
+   const templateParams = {
+     nombres: form.value.nombres,
+     telefono: form.value.telefono,
+     email: form.value.email,
+     direccion: form.value.direccion,
+     mensaje: form.value.mensaje,
+   };
+
+   // Enviar correo con EmailJS
+   emailjs
+ .send(
+   'service_s8fcqjj', // Reemplaza con tu Service ID
+   'template_tmyulpr', // Reemplaza con tu Template ID
+   templateParams,
+   'CMWTH4Y0D5vdGksWw' // Reemplaza con tu Public Key
+ )
+ .then(
+   (response) => {
+     console.log('Correo enviado con éxito:', response.status, response.text);
+     alert('Correo enviado correctamente.');
+     this.onSubmit(); // Redirige a Home después de enviar el correo
+   },
+   (error) => {
+     console.error('Error al enviar el correo:', error);
+     alert('Hubo un error al enviar el correo. Inténtalo nuevamente.');
+   }
+ );
+
+ }
+
+ // Método para redirigir a la página de inicio
+ onSubmit() {
+   this.router.navigate(['/home']);  // Redirige a Home
+ }
 }
