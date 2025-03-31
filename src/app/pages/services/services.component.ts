97ed26c0802df767a5a6991cae7fa445a75c8240
lchipeachy <bvlucia34@gmail.com>
2025-03-31 15:02:35 -0600

    cambios en textos

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -12,11 +12,11 @@ export class ServicesComponent {
     { icon: 'medication', title: 'Tratamientos Oncológicos', description: 'Aplicación de quimioterapia, radioterapia e inmunoterapia.' },
     { icon: 'healing', title: 'Cirugía Oncológica', description: 'Procedimientos quirúrgicos especializados en oncología.' },
     { icon: 'fact_check', title: 'Segunda Opinión Médica', description: 'Revisión de diagnósticos para una mejor decisión de tratamiento.' },
-    { icon: 'healing', title: 'Cáncer de mama', description: 'Biopsias' },
+    { icon: 'healing', title: 'Cáncer de mama', description: 'Biopsias.' },
     { icon: 'healing', title: 'Cáncer gastrointestinal' },
-    { icon: 'healing', title: 'Cáncer de Piel', description: 'Melanoma' },
+    { icon: 'healing', title: 'Cáncer de Piel', description: 'Melanoma.' },
     { icon: 'healing', title: 'Sarcoma de Tejidos Blandos' },
     { icon: 'healing', title: 'Tumores de Vías Urinarias'},
-    { icon: 'healing', title: 'Tumores Ginecológicos', description: 'Ovario, útero' },
+    { icon: 'healing', title: 'Tumores Ginecológicos', description: 'Ovario, útero.' },
   ];
 }
