faa0f16460370d7972e5f2e3696a2a6344bb17e3
lchipeachy <bvlucia34@gmail.com>
2025-03-31 15:48:59 -0600

    eliminando card

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -11,7 +11,6 @@ export class ServicesComponent {
     { icon: 'search', title: 'Detección Temprana', description: 'Pruebas y estudios preventivos para la detección temprana del cáncer.' },
     { icon: 'medication', title: 'Tratamientos Oncológicos', description: 'Aplicación de quimioterapia, radioterapia e inmunoterapia.' },
     { icon: 'healing', title: 'Cirugía Oncológica', description: 'Procedimientos quirúrgicos especializados en oncología.' },
-    { icon: 'fact_check', title: 'Segunda Opinión Médica', description: 'Revisión de diagnósticos para una mejor decisión de tratamiento.' },
     { icon: 'healing', title: 'Cáncer de mama', description: 'Biopsias.' },
     { icon: 'healing', title: 'Cáncer gastrointestinal' },
     { icon: 'healing', title: 'Cáncer de Piel', description: 'Melanoma.' },
